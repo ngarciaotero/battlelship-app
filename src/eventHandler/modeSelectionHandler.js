3cf2838f95d028077f71cb2438b33f27cae90d3b
ngarciaotero <nasleygarciaotero@gmail.com>
2024-12-29 13:33:53 -0500

    feat(modeSelect): initialize game controller and players on mode select
    - create players based on selected game mode and initialize game controller
    - allow access to the initialized game controller

--- src/eventHandler/modeSelectionHandler.js
+++ src/eventHandler/modeSelectionHandler.js
@@ -1,11 +1,15 @@
 import { createPlayerSection } from "../ui/playerSectionUI.js";
 import { displayToggle } from "../ui/helpers/displayToggle.js";
 import { placementStateManager } from "../ui/stateManagers/placementStateManager.js";
+import { createPlayer } from "../factories/player.js";
+import { createGameController } from "../game/gameController.js";
 
 const MODES = {
   HUMAN_VS_COMPUTER: "hvc",
   HUMAN_VS_HUMAN: "hvh",
 };
+
+let gameController = null;
 
 export const handleModeSelect = (mode) => {
   const playerOneContainer = document.querySelector(".player-one-section");
@@ -21,16 +25,32 @@ export const handleModeSelect = (mode) => {
   playerTwoContainer.appendChild(playerTwoSection);
 
   if (mode === MODES.HUMAN_VS_COMPUTER) {
+    const playerOne = createPlayer("real");
+    const playerTwo = createPlayer("computer");
+
+    gameController = createGameController();
+    gameController.addPlayers([playerOne, playerTwo]);
+
     placementStateManager.setHumanPlacementMode(1);
     placementStateManager.setComputerPlacementMode(2);
     placementStateManager.togglePlayerOverlay(2, true);
 
     displayToggle.displayGameLayout();
   } else if (mode === MODES.HUMAN_VS_HUMAN) {
+    const playerOne = createPlayer("real");
+    const playerTwo = createPlayer("real");
+
+    gameController = createGameController();
+    gameController.addPlayers([playerOne, playerTwo]);
+
     placementStateManager.setHumanPlacementMode(1);
     placementStateManager.setHumanPlacementMode(2);
     placementStateManager.togglePlayerOverlay(2, true);
 
     displayToggle.displayGameLayout();
   }
+
+  return gameController;
 };
+
+export const getGameController = () => gameController;

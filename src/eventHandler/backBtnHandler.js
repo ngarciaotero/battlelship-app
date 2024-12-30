80be05e4baace73cbb382a69d25015e209da5009
ngarciaotero <nasleygarciaotero@gmail.com>
2024-12-29 22:20:04 -0500

    feat(backBtnHandler): add logic to ui update for back button
    - reset the game state in game controller
    - returns to the mode selection menu
    - triggers the start button to refresh its current state

--- src/eventHandler/backBtnHandler.js
+++ src/eventHandler/backBtnHandler.js
@@ -1,5 +1,11 @@
 import { displayToggle } from "../ui/helpers/displayToggle.js";
+import { getGameController } from "./modeSelectionHandler.js";
+import { updateStartButtonUI } from "../ui/updateStartBtnUI.js";
 
 export const handleBackButton = () => {
+  const gameController = getGameController();
+  gameController.endPreGame();
+
   displayToggle.displayModeMenu();
+  updateStartButtonUI();
 };

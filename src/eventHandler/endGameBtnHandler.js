24889f484b56de9b3080dec342d9e39611d61353
ngarciaotero <nasleygarciaotero@gmail.com>
2024-12-31 16:19:34 -0500

    feat(endGameHandler): handle end game logic and ui updates
    - call end game from the game controller
    - hide game layout and display mode menu
    - update the start button ui to reflect game over state

--- src/eventHandler/endGameBtnHandler.js
+++ src/eventHandler/endGameBtnHandler.js
@@ -1,5 +1,11 @@
 import { displayToggle } from "../ui/helpers/displayToggle.js";
+import { getGameController } from "./modeSelectionHandler.js";
+import { updateStartButtonUI } from "../ui/updateStartBtnUI.js";
 
 export const handleEndGameButton = () => {
+  const gameController = getGameController();
+  gameController.endGame();
+
   displayToggle.displayModeMenu();
+  updateStartButtonUI();
 };

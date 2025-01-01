e52b8797f93bdea710c33998c2c57555c1654955
ngarciaotero <nasleygarciaotero@gmail.com>
2025-01-01 14:45:40 -0500

    refactor: consolidate board and game state setup into
    - removed boardUIHandler and cellUIHandler imports
    - moved board and attack listener setup, along with game mode initialization, into game state manager's setupGameMode

--- src/eventHandler/startGameBtnHandler.js
+++ src/eventHandler/startGameBtnHandler.js
@@ -1,17 +1,9 @@
 import { gameStateManager } from "../ui/stateManagers/gameStateManager.js";
 import { getGameController } from "./modeSelectionHandler.js";
-import { boardUIHandler } from "../ui/stateManagers/boardStateManager.js";
-import { cellUIHandler } from "../ui/stateManagers/cellStateManager.js";
 
 export const handleStartGameButton = () => {
   const gameController = getGameController();
   gameController.initializeGame();
 
-  // clear both player boards
-  boardUIHandler.clearBoardsUI();
-  cellUIHandler.setupAttackListeners();
-
-  gameStateManager.updateTurnOverlay();
-  gameStateManager.setGameMode(1);
-  gameStateManager.setGameMode(2);
+  gameStateManager.setupGameMode();
 };

3b0e96e22f34388384f5836f8dd264f673a5de3e
ngarciaotero <nasleygarciaotero@gmail.com>
2025-01-01 14:57:37 -0500

    refactor: make call to setupPlacementMode to handle mode selection
    - mode select logic moved in to placement state manager's setupPlacementMode

--- src/eventHandler/modeSelectionHandler.js
+++ src/eventHandler/modeSelectionHandler.js
@@ -1,57 +1,8 @@
-import { createPlayerSection } from "../ui/playerSectionUI.js";
-import { displayToggle } from "../ui/helpers/displayToggle.js";
 import { placementStateManager } from "../ui/stateManagers/placementStateManager.js";
-import { createPlayer } from "../factories/player.js";
-import { createGameController } from "../game/gameController.js";
-
-const MODES = {
-  HUMAN_VS_COMPUTER: "hvc",
-  HUMAN_VS_HUMAN: "hvh",
-};
-
-let gameController = null;
 
 export const handleModeSelect = (mode) => {
-  const playerOneContainer = document.querySelector(".player-one-section");
-  const playerTwoContainer = document.querySelector(".player-two-section");
-
-  const playerOneSection = createPlayerSection(1);
-  const playerTwoSection = createPlayerSection(2);
-
-  playerOneContainer.innerHTML = "";
-  playerTwoContainer.innerHTML = "";
-
-  playerOneContainer.appendChild(playerOneSection);
-  playerTwoContainer.appendChild(playerTwoSection);
-
-  if (mode === MODES.HUMAN_VS_COMPUTER) {
-    const playerOne = createPlayer("real");
-    const playerTwo = createPlayer("computer");
-
-    gameController = createGameController();
-    gameController.addPlayers([playerOne, playerTwo]);
-
-    placementStateManager.setHumanPlacementMode(1);
-    placementStateManager.setComputerPlacementMode(2);
-    placementStateManager.togglePlayerOverlay(2);
-
-    displayToggle.displayGameLayout();
-  } else if (mode === MODES.HUMAN_VS_HUMAN) {
-    const playerOne = createPlayer("real");
-    const playerTwo = createPlayer("real");
-
-    gameController = createGameController();
-    gameController.addPlayers([playerOne, playerTwo]);
-
-    placementStateManager.setHumanPlacementMode(1);
-    placementStateManager.setHumanPlacementMode(2);
-    placementStateManager.togglePlayerOverlay(2);
-    placementStateManager.setPassScreen();
-
-    displayToggle.displayGameLayout();
-  }
-
-  return gameController;
+  return placementStateManager.setupPlacementMode(mode);
 };
 
-export const getGameController = () => gameController;
+export const getGameController = () =>
+  placementStateManager.getGameController();

ebce6617a9adccc316f33fb136dfeb5872bae067
ngarciaotero <nasleygarciaotero@gmail.com>
2025-01-04 15:41:32 -0500

    feat: reset ship dock on clear button click

--- src/eventHandler/clearBoardBtnHandler.js
+++ src/eventHandler/clearBoardBtnHandler.js
@@ -1,10 +1,12 @@
 import { boardUIHandler } from "../ui/stateManagers/boardStateManager.js";
 import { getPlayer } from "../utils/playerUtils.js";
+import { dragDropUIHandler } from "../ui/stateManagers/dragAndDropStateManager.js";
 
 export const handleClearBoard = (suffix) => {
   const currentPlayer = getPlayer(suffix);
   if (!currentPlayer) return;
   currentPlayer.clearBoard();
+  dragDropUIHandler.resetDockShips(suffix);
 
   boardUIHandler.clearShipsFromBoard(suffix, currentPlayer);
 };

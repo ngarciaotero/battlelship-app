979943b8a59f0786b4acf1a2f6515454a8de4f34
ngarciaotero <nasleygarciaotero@gmail.com>
2025-01-04 15:42:14 -0500

    feat: reset ship dock when new board is generated

--- src/eventHandler/randomBoardBtnHandler.js
+++ src/eventHandler/randomBoardBtnHandler.js
@@ -1,10 +1,12 @@
 import { boardUIHandler } from "../ui/stateManagers/boardStateManager.js";
 import { getPlayer } from "../utils/playerUtils.js";
+import { dragDropUIHandler } from "../ui/stateManagers/dragAndDropStateManager.js";
 
 export const handleRandomPopulation = (suffix) => {
   const currentPlayer = getPlayer(suffix);
   if (!currentPlayer) return;
 
+  dragDropUIHandler.resetDockShips(suffix);
   currentPlayer.clearBoard();
   currentPlayer.populateBoard();
   boardUIHandler.randomlyPopulateBoard(suffix, currentPlayer);
